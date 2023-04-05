f0d4dcaf05f59dd1510ea7034dcfd2da0463e4c6
Anne Wang <huiyang.wang621@gmail.com>
2023-04-05 16:54:14 +1200

    fix: a

--- Formula/kinetik.rb
+++ Formula/kinetik.rb
@@ -4,10 +4,10 @@ class Kinetik < Formula
   version "0.13.5"
 
   if Hardware::CPU.arm?
-    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.0/kinetik-cli--0.13.5.arm64_monterey.bottle.tar.gz"
+    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.0/kinetik--0.13.5.arm64_monterey.bottle.tar.gz"
     sha256 "04f71a4e4f736d7247a934740e91a199111c52522ce5c12854e688edfa154296"
   else
-    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.0/kinetik-cli--0.13.5.x86_64_monterey.bottle.tar.gz"
+    url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.0/kinetik--0.13.5.x86_64_monterey.bottle.tar.gz"
     sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
   end
 
