66a1f3b6366d26648e51a604939090e05647f195
Anne Wang <huiyang.wang621@gmail.com>
2023-04-14 11:47:12 +1200

    fix: a

--- Formula/kinetik-cli.rb
+++ Formula/kinetik-cli.rb
@@ -9,7 +9,7 @@ class KinetikCli < Formula
     sha256 "04f71a4e4f736d7247a934740e91a199111c52522ce5c12854e688edfa154296"
   else
     url "https://github.com/xiaoanne/homebrew-d3/releases/download/v1.0.5/kinetik-cli--0.13.5.x86_64_linux.bottle.1.tar.gz"
-    sha256 "1be3b6d1146641dc024f1d79e1e48eb3995eab300f126fe25f54fe8a5098c06e"
+    sha256 "b8e8078427a79515d33fe5863ea0e8deb915b98d4b3e095f8e228444eb82ba0f"
   end
 
   def install
